87feb0129b69a68b6159709d252f53159ce29cbf
Göksu Yıldırım <goksuyildirim@gmail.com>
2016-05-08 20:09:06 +0300

    "Shoot closest minion on tap" added
    Scene view'e de bir tane PlayerController (empty gameobject) ekledim

---
+++ IdleTowerDefence/Assets/Scripts/PlayerSpell.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpell : MonoBehaviour {
+
+	public float spellSpeed = 0.001f;
+	public int damage = 20;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	//Update is called once per frame
+	void Update () {
+		GameObject targetMinion = FindClosestMinion ();
+		Vector3 spellTarget = targetMinion.transform.position - transform.position;
+		transform.Translate(spellTarget*spellSpeed*Time.deltaTime);
+	}
+
+	// Find closest minion's name
+	GameObject FindClosestMinion(){
+		GameObject[] minions;
+		minions = GameObject.FindGameObjectsWithTag("Minion");
+		GameObject closestMinion = null;
+		float distance = Mathf.Infinity;
+		Vector3 position = transform.position;
+		foreach (GameObject minion in minions) {
+			Vector3 diff = minion.transform.position - position;
+			float curDistance = diff.sqrMagnitude;
+			if (curDistance < distance) {
+				closestMinion = minion;
+				distance = curDistance;
+			}
+		}
+
+		return closestMinion;
+	}
+
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.tag=="Minion") {
+			Destroy (gameObject);
+			coll.gameObject.GetComponent<Minion> ().Life = coll.gameObject.GetComponent<Minion> ().Life - damage;
+
+
+		}
+	}
+
+
+}
