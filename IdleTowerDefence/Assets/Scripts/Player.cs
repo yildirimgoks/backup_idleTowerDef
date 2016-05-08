cc3bf091ef5f06ed7307ef908ebf37d1cc17c0e4
Göksu Yıldırım <goksuyildirim@gmail.com>
2016-05-08 21:13:49 +0300

    PlayerController removed / TapShooting merged to Player

--- IdleTowerDefence/Assets/Scripts/Player.cs
+++ IdleTowerDefence/Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public Rigidbody playerSpellPrefab;
     private int Currency = 0;
 
 	// Use this for initialization
@@ -12,7 +13,13 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 mousePos = Input.mousePosition;
+			mousePos.z = 8f;
+			Vector3 instantPos = Camera.main.ScreenToWorldPoint (mousePos);
+			Rigidbody playerSpellInstance;
+			playerSpellInstance = Instantiate (playerSpellPrefab, instantPos, Quaternion.identity) as Rigidbody;
+		}
 	}
 
     public void IncreaseCurrency(int amount)
