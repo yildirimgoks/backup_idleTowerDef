ff90f51dde19253920e41a631edb705a247f5b4a
Okan Agca <okanagca@gmail.com>
2016-05-08 23:06:47 +0200

    Add dummy button for test

--- IdleTowerDefence/Assets/UITestAssets/UITestScript.cs
+++ IdleTowerDefence/Assets/UITestAssets/UITestScript.cs
@@ -12,8 +12,9 @@ public class UITestScript : MonoBehaviour {
 	void Update () {
 	
 	}
-
+	 
     void OnGUI() {
         GUI.Label(new Rect(10, 10, 200, 20), "Hello World");
+		GUI.Button (new Rect (20, 40, 200, 20), "Ben bir tuşum");
     }
 }
