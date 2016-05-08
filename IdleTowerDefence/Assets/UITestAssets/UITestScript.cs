18a02ec32fed1248a6434e2a7eac7090a8199921
Okan Agca <okanagca@gmail.com>
2016-05-08 22:29:45 +0200

    add test ui scene for ui testing

---
+++ IdleTowerDefence/Assets/UITestAssets/UITestScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UITestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI() {
+        GUI.Label(new Rect(10, 10, 200, 20), "Hello World");
+    }
+}
