70580c292a341b57882500d7b401088c6d90c472
Uygar Tarakci <uygartarakci53@gmail.com>
2016-05-28 14:34:57 +0200

    Drag & Drop

--- IdleTowerDefence/Assets/Scripts/Mage.cs
+++ IdleTowerDefence/Assets/Scripts/Mage.cs
@@ -8,6 +8,8 @@ namespace Assets.Scripts
         public float delay;
         private float spellTime = 0.0f;
 
+        public bool active = false;  
+
         // Use this for initialization
         private void Start()
         {
@@ -17,11 +19,32 @@ namespace Assets.Scripts
         private void Update()
         {
             // Cast spell with delay
-            if (Time.time > spellTime)
+            if (Time.time > spellTime && active)
             {
                 spellTime = Time.time + delay;
                 Instantiate(TowerSpellPrefab, gameObject.transform.position, Quaternion.identity);
             }
         }
+
+         private Vector3 screenPoint;
+         private Vector3 offset;
+ 
+        void OnMouseDown()
+        {
+            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+ 
+             offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+ 
+         }
+ 
+         void OnMouseDrag()
+         {
+             Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+ 
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+            transform.position = curPosition;
+ 
+         }
+
     }
 }
