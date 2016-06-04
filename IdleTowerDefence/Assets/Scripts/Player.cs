7946ea7d9644a02ea6ac51af1a3ff632d2f2bc6f
uelker <mertulker14@gmail.com>
2016-06-04 11:38:27 +0200

    Variable "New" changed to "Dropped".

--- IdleTowerDefence/Assets/Scripts/Player.cs
+++ IdleTowerDefence/Assets/Scripts/Player.cs
@@ -90,7 +90,7 @@ namespace Assets.Scripts
                 if (minion.tag == "Boss")
                 {
                     Mage newMage = Instantiate(MagePrefab, new Vector3(minion.transform.position.x, 12.2f, minion.transform.position.z), Quaternion.Euler(0, 0, 90)) as Mage;
-                    newMage.New = true;
+                    newMage.Dropped = true;
                     Time.timeScale = 0;
                 }
             }
