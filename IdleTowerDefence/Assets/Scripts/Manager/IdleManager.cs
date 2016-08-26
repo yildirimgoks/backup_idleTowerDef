93b38d481d7bf21563354b98a1c5dd2a09a089eb
Okan Agca <okanagca@gmail.com>
2016-08-26 23:44:41 +0200

    Simplify idle manager

--- IdleTowerDefence/Assets/Scripts/Manager/IdleManager.cs
+++ IdleTowerDefence/Assets/Scripts/Manager/IdleManager.cs
@@ -6,12 +6,9 @@ namespace Assets.Scripts.Manager
     public class IdleManager
     {
         //Idle Income Calculation
-        private BigIntWithUnit _maxPotentialWaveDmg;
-        private BigIntWithUnit _mageDps;
         private readonly int _roadLength = 290; //calculated as distances between each waypoint
         private readonly Player _player;
         private readonly WaveManager _waveManager;
-        private readonly Mage _mage;
 
         public IdleManager(Player player, WaveManager waveManager)
         {
@@ -22,74 +19,47 @@ namespace Assets.Scripts.Manager
         public BigIntWithUnit CalculateIdleIncome(out int killedCreatures, out int passedLevels)
         {
             var gameCloseTime = PlayerPrefs.GetString("_gameCloseTime");
-            var gameClosedAt = DateTime.Parse(gameCloseTime);
-            var now = DateTime.Now;
-            var idleTime = now - gameClosedAt;
+            var idleTime = DateTime.Now - DateTime.Parse(gameCloseTime);
             var idleTimeInSeconds = idleTime.TotalSeconds;
 			var mageAttackDuration = _roadLength / (int)_waveManager.WaveSpeed;
             BigIntWithUnit totalIncome = 0;
             killedCreatures = 0;
             passedLevels = 0;
             //Calculate Total Idle Damage
-            _mageDps = _player.Data.CumulativeDps();
-            _maxPotentialWaveDmg = _mageDps * mageAttackDuration;
+            var maxPotentialWaveDmg = _player.Data.CumulativeDps() * mageAttackDuration;
 
-            //Establish Idle Currency Formula
-            var multiplierMoney = Math.Pow(1.03, _waveManager.Data.CurrentWave); // %30 money multiplier
-            var currencyGained = UpgradeManager.MinionDeathRewardInitial * multiplierMoney;
-            currencyGained = currencyGained * _waveManager.Data.GetCurrentWaveLength();
-            
-            //find the idle mage count and give currency for them beforehand
-            foreach (var mage in _player.Data.GetMages())
-            {
-                if (mage.Data.IsIdle())
-                {
-                    totalIncome += mage.Data.GetIdleCurrency()*(int)idleTimeInSeconds;
-                }
-            }
+            //Calculate the idle mage currency
+            totalIncome += _player.Data.CumulativeIdleEarning() * idleTimeInSeconds;
 
             //Idle Currency Gaining
-            for (int i = 0; i < 5; i++)
+            while (idleTimeInSeconds > 0)
             {
+                var timeMultiplier = 1.0;
                 if (idleTimeInSeconds < mageAttackDuration) //not enough time left to kill the wave
                 {
-                    break;
+                    timeMultiplier = idleTimeInSeconds/mageAttackDuration;
                 }
-                var waveKilled = _maxPotentialWaveDmg / _waveManager.WaveLife >= 1;
-                if (waveKilled && !_waveManager.Data.IsNextWaveBossWave)
+                var waveKilledPercent = maxPotentialWaveDmg / _waveManager.WaveLife;
+                totalIncome += GetCurrencyGainedForWave() * waveKilledPercent * timeMultiplier;
+                killedCreatures += (int)(_waveManager.Data.GetCurrentWaveLength() * waveKilledPercent * timeMultiplier);
+                idleTimeInSeconds -= mageAttackDuration;
+
+                if (waveKilledPercent >= 1 && !_waveManager.Data.IsNextWaveBossWave) //calculate currency gain of next wave if it comes
                 {
-                    passedLevels ++;
+                    passedLevels++;
                     _waveManager.Data.IncreaseCurrentWaveAndMaxWave();
                     _waveManager.SendWave();
                 }
-                currencyGained = BigIntWithUnit.MultiplyPercent(currencyGained, _maxPotentialWaveDmg / _waveManager.WaveLife*100);
-                totalIncome += currencyGained;
-                idleTimeInSeconds -= mageAttackDuration;
-                if (waveKilled) //calculate currency gain of next wave if it comes
-                {
-                    currencyGained = UpgradeManager.MinionDeathRewardInitial * multiplierMoney;
-                    currencyGained = currencyGained * _waveManager.Data.GetCurrentWaveLength();
-                    killedCreatures += _waveManager.Data.GetCurrentWaveLength();
-                }
-                else //next wave can't come 
-                {
-                    break;
-                }
-                if (_waveManager.Data.IsNextWaveBossWave) //next wave is boss, give money of the last wave continuously until time runs out
-                {
-                    Debug.Log("A Boss is attacking your castle!");
-                    break; //send notification
-                }
-            }
-
-            while (idleTimeInSeconds > mageAttackDuration)
-            {
-                //Calculate end currency amount and return
-                totalIncome += currencyGained;
-                idleTimeInSeconds -= mageAttackDuration;
-                killedCreatures += _waveManager.Data.GetCurrentWaveLength();
             }
             return totalIncome;
+        }
+
+        private BigIntWithUnit GetCurrencyGainedForWave()
+        {
+            //Establish Idle Currency Formula
+            var multiplierMoney = Math.Pow(UpgradeManager.MinionDeathRewardMultiplier, _waveManager.Data.CurrentWave);
+            var singleCreepReward = UpgradeManager.MinionDeathRewardInitial * multiplierMoney;
+            return singleCreepReward * _waveManager.Data.GetCurrentWaveLength();
         }
     }
 }
