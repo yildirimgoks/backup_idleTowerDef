6f05a1f0839943c3a5cf50b5af3c188083730a6a
Okan Agca <okanagca@gmail.com>
2016-08-27 00:05:40 +0200

    Structure constants

--- IdleTowerDefence/Assets/Scripts/Manager/UpgradeManager.cs
+++ IdleTowerDefence/Assets/Scripts/Manager/UpgradeManager.cs
@@ -2,19 +2,32 @@
 {
     public class UpgradeManager
     {
-        public static float MinionDeathRewardMultiplier = 1.10f;
-        public static float MageUpgradePriceMultiplier = 1.14f;
-        public static float MinionLifeMultiplier = 1.12f;
-        //See Element Manager
-        public static float MageDamageMultiplier = 1.10f;
-        //public static float MageFireRateMultiplier = 1.05f;
-        public static float MageIdleGeneratedMultiplier = 1.07f;
+        #region MinionConstants
 
         public static BigIntWithUnit MinionDeathRewardInitial = 10;
-        public static BigIntWithUnit MageUpgradePriceInitial = 30;
+        public static float MinionDeathRewardMultiplier = 1.10f;
+        
         public static BigIntWithUnit MinionLifeInitial = 100;
-        public static BigIntWithUnit MageDamageInitial = 50;
-        public static float MageFireRateInitial = 1.0f;
+        public static float MinionLifeMultiplier = 1.12f;
+
+        #endregion
+
+        #region MageConstants
+        public static float MageIdleGeneratedMultiplier = 1.07f;
         public static BigIntWithUnit MageIdleGenerationInitial = 2;
+
+        public static BigIntWithUnit MageUpgradePriceInitial = 30;
+        public static float MageUpgradePriceMultiplier = 1.14f;
+
+        #region ElementConstants
+        //ToDo: Add elemental constants
+        public static BigIntWithUnit MageDamageInitial = 50;
+        public static float MageDamageMultiplier = 1.10f;
+        public static float MageFireRateInitial = 1.0f;
+        public static float MageFireRateMultiplier = 1.05f;
+        #endregion
+
+        #endregion
+
     }
 }
