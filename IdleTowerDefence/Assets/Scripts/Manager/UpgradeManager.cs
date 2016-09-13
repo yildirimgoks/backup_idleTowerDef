9a4a4be3a09ab37676ad911a235bbe293bdfe131
goksuyildirim <goksuyildirim@gmail.com>
2016-09-13 14:14:15 +0300

    New Fireball Sound Effect
    Proper wind effect is impossible to find :(

--- IdleTowerDefence/Assets/Scripts/Manager/UpgradeManager.cs
+++ IdleTowerDefence/Assets/Scripts/Manager/UpgradeManager.cs
@@ -28,21 +28,21 @@
 
         public static BigIntWithUnit MageFireDamageInitial = 120;
         public static float MageFireDamageMultiplier = 1.15f;
-        public static float MageFireRangeInitial = 10.0f;
+        public static float MageFireRangeInitial = 15.0f;
         public static float MageFireRangeMultiplier = 1.05f;
         public static float MageFireRateInitial = 2.0f;
         public static float MageFireRateMultiplier = 1.04f;
 
         public static BigIntWithUnit MageWaterDamageInitial = 60;
         public static float MageWaterDamageMultiplier = 1.04f;
-        public static float MageWaterRangeInitial = 10.0f;
+        public static float MageWaterRangeInitial = 15.0f;
         public static float MageWaterRangeMultiplier = 1.15f;
         public static float MageWaterRateInitial = 1.0f;
         public static float MageWaterRateMultiplier = 1.05f;
 
         public static BigIntWithUnit MageEarthDamageInitial = 120;
         public static float MageEarthDamageMultiplier = 1.15f;
-        public static float MageEarthRangeInitial = 5.0f;
+        public static float MageEarthRangeInitial = 15.0f;
         public static float MageEarthRangeMultiplier = 1.04f;
         public static float MageEarthRateInitial = 1.0f;
         public static float MageEarthRateMultiplier = 1.05f;
