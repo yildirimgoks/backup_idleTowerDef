2b7b734fdc973724244889982ac30ad608181ca7
Berke Tezergil <btezergil@hotmail.com>
2016-12-05 00:05:49 +0300

    Fixed popup damage text not showing true dmg value while having bonus

--- IdleTowerDefence/Assets/Scripts/Minion.cs
+++ IdleTowerDefence/Assets/Scripts/Minion.cs
@@ -104,7 +104,7 @@ namespace Assets.Scripts
         {
             var dmg = Data.DecreaseLife(damage*_controller.GetDamageModifier());
             var height = new Vector3(-5f, 12f, 0f);
-            _uiman.CreateFloatingText(damage.ToString(), transform, transform.position + height, "d");
+            _uiman.CreateFloatingText(dmg.ToString(), transform, transform.position + height, "d");
             return dmg;
         }
 
